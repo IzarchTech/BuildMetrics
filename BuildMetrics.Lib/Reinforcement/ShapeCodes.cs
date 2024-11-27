40ac3436a527a3266d5541d2bc06abb0c4300792
Chukwunwike Udezue <udezuec@protonmail.com>
2024-11-27 16:44:58 +0100

    Feat: Bar bending shape copes

---
+++ BuildMetrics.Lib/Reinforcement/ShapeCodes.cs
@@ -0,0 +1,87 @@
+namespace BuildMetrics.Lib.Reinforcement;
+
+/// <summary>
+/// Bar bending shape codes
+/// </summary>
+public enum ShapeCodes
+{
+    /// <summary>
+    /// 00
+    /// </summary>
+    Code00,
+    
+    /// <summary>
+    /// 11
+    /// </summary>
+    Code11,
+    
+    /// <summary>
+    /// 12
+    /// </summary>
+    Code12,
+    
+    /// <summary>
+    /// 13
+    /// </summary>
+    Code13,
+    
+    /// <summary>
+    /// 15
+    /// </summary>
+    Code15,
+    
+    /// <summary>
+    /// 21
+    /// </summary>
+    Code21,
+    
+    /// <summary>
+    /// 25
+    /// </summary>
+    Code25,
+    
+    /// <summary>
+    /// 26
+    /// </summary>
+    Code26,
+    
+    /// <summary>
+    /// 31
+    /// </summary>
+    Code31,
+    
+    /// <summary>
+    /// 33
+    /// </summary>
+    Code33,
+    
+    /// <summary>
+    /// 41
+    /// </summary>
+    Code41,
+    
+    /// <summary>
+    /// 44
+    /// </summary>
+    Code44,
+    
+    /// <summary>
+    /// 46
+    /// </summary>
+    Code46,
+    
+    /// <summary>
+    /// 51
+    /// </summary>
+    Code51,
+    
+    /// <summary>
+    /// 67
+    /// </summary>
+    Code67,
+    
+    /// <summary>
+    /// 77
+    /// </summary>
+    Code77,
+}
